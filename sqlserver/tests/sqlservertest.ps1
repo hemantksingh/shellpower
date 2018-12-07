dbd77410fd252cd5f966af2487058a2ef19f2573
Hemant Kumar <hemantksingh.hk@gmail.com>
2018-12-07 21:18:42 +0000

    Simplify sqluser creation

--- sqlserver/tests/sqlservertest.ps1
+++ sqlserver/tests/sqlservertest.ps1
@@ -14,29 +14,56 @@ Write-Host "Importing from source $source"
 function Configure-SqlUser (
     [Parameter(mandatory=$true)][string] $dbServer,
     [Parameter(mandatory=$true)][string] $dbName,
-    [Parameter(mandatory=$true)][string] $dbUser,
-    [Parameter(Mandatory=$true)][System.Array] $serverRoles,
-    [Parameter(Mandatory=$true)][System.Array] $dbRoles) {
+    [Parameter(mandatory=$true)][PSObject]$sqlUser) {
     
     [reflection.assembly]::LoadWithPartialName("Microsoft.SqlServer.Smo")
     $server = new-object Microsoft.SqlServer.Management.Smo.Server($dbServer)
     
-    $userLogin = Create-Login $server $dbUser "test-passw0rd!"
-    Add-LoginToServerRoles $server $userLogin.Name $serverRoles
+    $userLogin = Create-Login $server $sqlUser.name $sqlUser.password
+    Add-LoginToServerRoles $server $userLogin.Name $sqlUser.serverRoles
 
     $db = Create-Db $server $dbName
-    Add-UserToDb $db $dbUser                                                         
-    Add-UserToDbRoles $db $dbUser $dbRoles
+    Add-UserToDb $db $sqlUser.name
+    Add-UserToDbRoles $db $sqlUser.name $sqlUser.dbRoles
 }
 
-function Test-DbConfigurationCanBeRepeated {
-    Configure-SqlUser $dbServer "foo" "bar" `
-        -serverRoles @("dbcreator", "bulkadmin", "sysadmin") `
-        -dbRoles @("db_datareader", "db_datawriter")
+function Create-SqlUser (
+    [Parameter(mandatory=$true)][string] $name,
+    [Parameter(mandatory=$true)][string] $password,
+    [System.Array] $serverRoles,
+    [System.Array] $dbRoles) {
     
-    Configure-SqlUser $dbServer "foo" "bar" `
-        -serverRoles @("dbcreator", "bulkadmin", "sysadmin") `
-        -dbRoles @("db_ddladmin", "db_owner")
+    if(!$serverRoles) { 
+        Write-Host "No server role(s) specified" 
+        $serverRoles = @("dbcreator")
+    }
+    if(!$dbRoles) { 
+        Write-Host "No db role(s) specified" 
+        $dbRoles = @("db_datareader")
+    }
+
+    $sqlUser = New-Object -TypeName PSObject
+    $sqlUser | Add-Member -Name 'name' -MemberType Noteproperty -Value $name
+    $sqlUser | Add-Member -Name 'password' -MemberType Noteproperty -Value $password
+    $sqlUser | Add-Member -Name 'serverRoles' -MemberType Noteproperty -Value $serverRoles
+    $sqlUser | Add-Member -Name 'dbRoles' -MemberType Noteproperty -Value $dbRoles
+    
+    return $sqlUser
 }
 
-Test-DbConfigurationCanBeRepeated
+function Test-SqlUserCanBeConfiguredWithRoles {
+    $user = Create-SqlUser "bar" "test-passw0rd!" `
+        -serverRoles @("dbcreator", "bulkadmin", "sysadmin") `
+        -dbRoles @("db_datareader", "db_datawriter", "db_ddladmin", "db_owner")
+  
+    Configure-SqlUser $dbServer -dbName "foo" $user
+    Configure-SqlUser $dbServer -dbName "foo" $user
+}
+
+function Test-SqlUserCanBeConfiguredWithoutSpecifyingRoles {
+    $user = Create-SqlUser "pod" "test-passw0rd!"
+    Configure-SqlUser $dbServer -dbName "foo" $user
+}
+
+Test-SqlUserCanBeConfiguredWithRoles
+Test-SqlUserCanBeConfiguredWithoutSpecifyingRoles
