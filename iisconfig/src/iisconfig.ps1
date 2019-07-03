c39d707628ff5ee8ca5dc0fab2937112fc2781f2
Hemant Kumar <hemantksingh.hk@gmail.com>
2019-07-03 11:16:27 +0100

    Add website component

--- iisconfig/src/iisconfig.ps1
+++ iisconfig/src/iisconfig.ps1
@@ -4,67 +4,8 @@ Import-Module WebAdministration
 
 $currentDir = Split-Path $script:MyInvocation.MyCommand.Path
 . $currentDir\apppool.ps1
-. $currentDir\certificate.ps1
+. $currentDir\website.ps1
 
-function Create-Website (
-    [Parameter(mandatory = $true)][string] $name,
-    [Parameter(mandatory = $true)][int] $port,
-    [Parameter(mandatory = $true)][string] $appPool,
-    [Parameter(mandatory = $true)][string] $physicalPath,
-    [string] $username,
-    [string] $password,
-    [string] $protocol = 'http',
-    [string] $hostHeader = "$name.test.com",
-    [string] $certificateThumbprint) {
-
-    Write-Host "Creating website '$name' with appPool '$appPool' on port '$port' and path '$physicalPath'"
-    Create-AppPool -name $appPool -username $username -password $password
-    
-    New-Website `
-        -Name $name `
-        -Port $port  `
-        -PhysicalPath $physicalPath `
-        -ApplicationPool $appPool `
-        -Force
-    
-    Get-WebBinding -Name $name -Port $port | Remove-WebBinding
-    Write-Host "Adding web binding '$name' with protocol '$protocol', port '$port' and host header '$hostHeader' "
-    $webBinding = New-WebBinding -Name $name -IPAddress "*" -Port $port -Protocol $protocol -HostHeader $hostHeader
-    Write-Host "Waiting for 2 seconds for the web binding to take effect"; Start-Sleep 2 
-    # MS recommends waiting after adding a new web binding
-    # https://docs.microsoft.com/en-us/powershell/module/webadminstration/remove-webbinding?view=winserver2012-ps
-
-    if (![string]::IsNullOrWhiteSpace($certificateThumbrint)) {
-        Add-InstalledCertificateToBinding $certificateThumbrint $webBinding
-    }
-
-    Write-Host "Starting website '$name' ..."
-    Start-Website -Name $name
-}
-
-function Delete-Website(
-    [Parameter(mandatory = $true)]
-    [string] $name) {
-
-    $exists = (Get-Website -Name $name).Name -eq $name
-
-    if ($exists) {
-        Write-Warning "Website '$name' already exists, deleting it"
-        Remove-WebSite -Name $name
-    }
-    else {
-        Write-Host "Website '$name' not found, nothing deleted"
-    }
-}
-
-
-function Ensure-SiteExists([Parameter(mandatory = $true)][string] $siteName) {
-  
-    if ((Get-Website -Name $siteName).Name -ne $siteName) {
-        $message = "Failed to create web application '$name', website '$siteName' was not found"
-        throw [System.InvalidOperationException] $message
-    }
-}
 function Create-WebApplication (
     [Parameter(mandatory = $true)][string] $name,
     [Parameter(mandatory = $true)][string] $siteName,
@@ -94,23 +35,7 @@ function Create-WebApplication (
         -Force
 }
 
-function Parse-SitePath( 
-    [Parameter(mandatory = $true)][string] $sitePath,
-    [Parameter(mandatory = $true)][string] $webappName) {
-    
-    $siteParts = $sitePath.Split("/"); $site = $siteParts[0]; $virDir = $siteParts[1]
-    if ($siteParts.Length -gt 0) {
-        @{
-            site    = $site
-            webapp = "{0}/{1}" -f $virDir, $webappName
-        }
-    } else {
-        @{
-            site   = $sitePath
-            webapp = $webappName
-        }
-    }
-}
+
 function Delete-WebApplication(  
     [Parameter(mandatory = $true)][string] $name,
     [Parameter(mandatory = $true)][string] $siteName) {
