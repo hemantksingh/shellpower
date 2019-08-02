d05176357a6c8363a5859a4020a544a3e7689c31
Hemant Kumar <hemantksingh.hk@gmail.com>
2019-08-02 17:59:00 +0100

    Use ip add param

--- iisconfig/src/lib/webbinding.ps1
+++ iisconfig/src/lib/webbinding.ps1
@@ -7,7 +7,7 @@ function Add-WebBinding (
     
     Get-WebBinding -Name $siteName -Port $port | Remove-WebBinding
     Write-Host "Adding web binding to site '$siteName' with protocol '$protocol', port '$port' and host name '$hostName' "
-    New-WebBinding -Name $siteName -IPAddress "*" -Port $port -Protocol $protocol -HostHeader $hostName
+    New-WebBinding -Name $siteName -IPAddress $ipAddress -Port $port -Protocol $protocol -HostHeader $hostName
     Write-Host "Waiting for 2 seconds for the web binding to take effect"; Start-Sleep 2 
     # MS recommends waiting after adding a new web binding
     # https://docs.microsoft.com/en-us/powershell/module/webadminstration/remove-webbinding?view=winserver2012-ps
