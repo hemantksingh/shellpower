0b8e144d8dd279c6402121769ad9cdcb841986a0
Hemant Kumar <hemantksingh.hk@gmail.com>
2019-12-02 17:55:48 +0000

    Add tests for getting hosts

--- iisconfig/src/lib/host.ps1
+++ iisconfig/src/lib/host.ps1
@@ -1,14 +1,18 @@
-$_fileName = 'C:\Windows\System32\drivers\etc\hosts'
+param (
+  [string] $hostFile = 'C:\Windows\System32\drivers\etc\hosts'
+)
+
+Write-Host "Using host file '$hostFile'"
 function Add-HostMapping(
     [Parameter(mandatory = $true)][string]$ip, 
     [Parameter(mandatory = $true)][string]$hostname) {
     
     Remove-HostMapping $hostname
-    $ip + "`t`t" + $hostname | Out-File -encoding ASCII -append $_fileName
+    $ip + "`t`t" + $hostname | Out-File -encoding ASCII -append $hostFile
 }
 
 function Remove-HostMapping([Parameter(mandatory = $true)][string]$hostname) {
-    $content = Get-Content $_fileName
+    $content = Get-Content $hostFile
     $existingLines = @()
 
     foreach ($line in $content) {
@@ -24,14 +28,31 @@ function Remove-HostMapping([Parameter(mandatory = $true)][string]$hostname) {
     }
 
     # Write file
-    Clear-Content $_fileName
+    Clear-Content $hostFile
     foreach ($line in $existingLines) {
-        $line | Out-File -encoding ASCII -append $_fileName
+        $line | Out-File -encoding ASCII -append $hostFile
     }
 }
 
+function Get-HostMapping([Parameter(mandatory = $true)][string]$hostname) {
+    $content = Get-Content $hostFile
+    $mappings = @()
+
+    foreach ($line in $content) {
+        $bits = [regex]::Split($line, "\t+")
+        if ($bits.count -eq 2 -and ($bits[1] -eq $hostname)) {
+            Write-Host "Host mapping found for '$($bits[1])'"
+            $mappings +=  @{
+                ip = $bits[0]
+                host = $bits[1]
+            }
+        }
+    }
+    return $mappings
+}
+
 function Print-Hosts() {
-    $content = Get-Content $_fileName
+    $content = Get-Content $hostFile
 
     foreach ($line in $content) {
         $bits = [regex]::Split($line, "\t+")
